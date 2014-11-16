524574c86d6c68405c3b9f2cb5b66b53f1721a90
matthew <up631914@myport.ac.uk>
2014-11-16 18:40:26 +0000

    Hidden Nav Bar from list.

--- app/src/main/java/mjay/launcher/HomeActivity.java
+++ app/src/main/java/mjay/launcher/HomeActivity.java
@@ -3,18 +3,28 @@ package mjay.launcher;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class HomeActivity extends Activity {
 
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
     }
     public void showApps(View v){
         Intent i = new Intent(this, AppsListActivity.class);
         startActivity(i);
     }
+
+
 }
